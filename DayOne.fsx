a889b3956610a9a8b094c78d6d04455ad2eb3537
Giorgio Vilardo <giorgio.vilardo@it.clara.net>
2021-12-01 23:24:12 +0100

    Day one part two

--- DayOne.fsx
+++ DayOne.fsx
@@ -14,10 +14,32 @@ let input =
        158
        147
        150
-       153 |]
+       153
+       156
+       166
+       171
+       172
+       174
+       186
+       193
+       198
+       203
+       217 |]
 
-let countRisingQuantities =
-    input
+let countRisingQuantities (i:int[]) =
+    i
     |> Array.pairwise
     |> Array.filter (fun (p1, p2) -> p2 > p1)
     |> Array.length
+
+printfn "%i" (countRisingQuantities input)
+
+let countRisingQuantitiesTriples (i:int[]) =
+    i
+    |> Array.windowed 3
+    |> Array.map Array.sum
+    |> Array.pairwise
+    |> Array.filter (fun (p1, p2) -> p2 > p1)
+    |> Array.length
+
+printfn "%i" (countRisingQuantitiesTriples input)
